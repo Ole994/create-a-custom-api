648ca99e603e3c44e710a5a5ba4019c77b77b8e2
Ole994 <69842859+Ole994@users.noreply.github.com>
2021-10-05 15:20:00 +0200

    first-commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # create-a-custom-api
+
+<!-- The ternary operator
+The ternary operator is a frequently used shorthand way to write an if/else statement. -->
---
+++ js/setting/script.js
@@ -0,0 +1,76 @@
+//the ternary operator
+// const products = [
+//     {
+//         type: "produt A",
+//         delivered: false
+//     },
+
+//     {
+//         type: "produt B",
+//         delivered: true
+//     },
+
+//     {
+//         type: "produt C",
+//         delivered: false
+//     },
+
+//     {
+//         type: "produt D",
+//         delivered: true
+//     },
+// ];
+
+// const container = document.querySelector(".product-container");
+
+// for (let i = 0; i < products.length; i++) {
+//     let colour = "blue";
+
+//     if (products[i].delivered) {
+//         colour = "green"
+//     }
+
+//     container.innerHTML += `<div class ="product" style ="background-color: ${colour}">
+//                                 ${products[i].type}   
+//                             </div>
+//     `
+// }
+
+
+
+
+
+
+
+//Object destructuring
+
+// const things = {
+//     id: 4657,
+//     name: "Great Gatsby",
+//     tags: ["tag a", "tag b", "tag c", "tag d",],
+//     nice: {
+//         country: "France"
+//     },
+// };
+
+
+// const log = (id, name, tags) =>{
+//     console.log (id);
+//     console.log (name);
+//     console.log (tags);
+
+   
+
+// } 
+
+// log (things)
+// const { id, name: nameThings } = things;
+
+// const name = things.name;
+
+// const { country } = things.nice;
+
+// console.log ("nice")
+
+
+ 
